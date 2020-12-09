7ca3fc7d779386441673a92f7271c9ece60d848d
Franco Ortega <francopdx@gmail.com>
2020-12-08 18:20:25 -0800

    add passing test for POST endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,17 @@
-//const fs = require('fs');
+const fs = require('fs');
 const request = require('supertest');
 const app = require('../app.js');
-//const pool = require('../lib/utils/pool.js');
+const pool = require('../lib/utils/pool.js');
 
 describe('app.js endpoints', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  })
+
 
   it('sample test to make sure the wiring is connected', () => {
     return request(app)
@@ -13,5 +21,24 @@ describe('app.js endpoints', () => {
       });
 
   });
+
+  it('creates a map via POST', async() => {
+    const resposne = await request(app)
+      .post('/map')
+      .send({
+        title: 'four',
+        terrain: 'swamp',
+        price: '100'
+      })
+    expect(resposne.body).toEqual({
+      id: '1',
+      title: 'four',
+      terrain: 'swamp',
+      price: '100'
+    });
+  });
+
 });
+
+
 
